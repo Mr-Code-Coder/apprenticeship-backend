2152ea4cfeb01936bd5adcf3dd04cba952f3db06
Mr-Code-Coder <dodgyfish101@gmail.com>
2026-03-26 17:02:23 +0000

    altered host

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ def get_all_jobs(session: Session = Depends(get_session)): # safely open a sessi
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="https://apprenticeship-backend.onrender.com", port=8000, reload=True)
